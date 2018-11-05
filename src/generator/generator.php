3fb1aefdbb66d5eb35c15c6c77360c034d34daa0
Rio Astamal <me@rioastamal.net>
2018-11-05 20:38:26 +0700

    Generator: Add try catch block

--- src/generator/generator.php
+++ src/generator/generator.php
@@ -11,15 +11,24 @@ function env($envName, $default = null)
     return $default;
 }
 
-$config =[
+$config = [
     'quranJsonDir' => env('QURAN_JSON_DIR'),
+    'baseUrl' => env('QURAN_BASE_URL'),
     'buildDir' => BASE_DIR . '/build',
     'publicDir' => BASE_DIR . '/src/public',
     'templateDir' => env('QURAN_TEMPLATE_DIR', BASE_DIR . '/src/generator/template'),
     'beginSurah' => env('QURAN_BEGIN_SURAH', 1),
     'endSurah' => env('QURAN_END_SURAH', 114),
+    'githubProjectUrl' => env('QURAN_GITHUB_PROJECT_URL', 'https://github.com/rioastamal/quran-web')
 ];
 
-$generator = new SurahGenerator($config);
-$generator->copyPublic();
-$generator->makeSurah();
+echo "Generating website...";
+try {
+    $generator = new SurahGenerator($config);
+    $generator->copyPublic();
+    $generator->makeSurah();
+    echo "done.\n";
+} catch (Exception $e) {
+    echo "FAIL.\n";
+    printf("Error: %s\n", $e->getMessage());
+}
